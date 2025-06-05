26f3f808a11faf1246c73328ffe7521408a3f2ee
Pedro Vinicius <159270159+vinPedro@users.noreply.github.com>
2025-06-05 16:11:55 -0300

    Atualização para conexão com projeto React Cost

--- src/main/java/com/costs/controller/impl/ProjetoController.java
+++ src/main/java/com/costs/controller/impl/ProjetoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/projetos")
 public class ProjetoController implements ProjetoControllerAPI {
